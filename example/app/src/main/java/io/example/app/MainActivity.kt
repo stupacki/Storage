df48fa430b67a09740cb44eb8135e6a8759457d4
Benny Schneider <stupacki86@gmail.com>
2019-11-09 13:38:25 +0100

    Init project

---
+++ example/app/src/main/java/io/example/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stupacki.storage.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
